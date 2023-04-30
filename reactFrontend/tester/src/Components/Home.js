023e5f41f2196902e309d064c2a9ec0bf4e70a3f
tanisq2312 <tanisq@bu.edu>
2023-04-30 16:29:21 -0400

    implemented song recommmendations based on weather

--- reactFrontend/tester/src/Components/Home.js
+++ reactFrontend/tester/src/Components/Home.js
@@ -6,7 +6,8 @@ import Logout from './Logout'
 function Home() {
     const [access_token, setAccessToken] = useState(null);
     const [tracks, setTracks] = useState([]);
-  
+    const [city, setCity] = useState(null);
+
     useEffect(() => {
       fetch('http://localhost:5001/access_token', { credentials: 'include' })
         .then(response => response.json())
@@ -16,10 +17,11 @@ function Home() {
         });
     }, []);
   
-    useEffect(() => {
-      if (access_token) {
+   useEffect(() => {
+      if (access_token && city) {
         console.log('access_token:', access_token);
-        fetch(`http://localhost:5001/home?access_token=${access_token}`)
+        console.log('city:', city);
+        fetch(`http://localhost:5001/home?access_token=${access_token}&city=${city}`)
           .then(response => response.json())
           .then(data => {
             console.log('data from API:', data);
@@ -28,22 +30,25 @@ function Home() {
             }
           });
       } else {
-        console.log("Access token is not available yet");
+        console.log("Access token or city is not available yet");
       }
-    }, [access_token]);
+    }, [access_token, city]);
     
+    const handleCityChange = (newCity) => {
+        setCity(newCity);
+    }
   
     return (
       <div>
         <div style={{ textAlign: 'right' }}>
           <Logout />
         </div>
-        <WeatherForm />
+        <WeatherForm access_token={access_token} onCityChange={handleCityChange} />
         {tracks.map(track => (
           <TrackButton key={track.name} track={track} />
           ))}
       </div>
     );
-  }
-  
-  export default Home;
+}
+
+export default Home;
